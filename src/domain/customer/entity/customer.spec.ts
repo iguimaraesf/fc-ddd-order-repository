d1df5a9ae525d4ad1c973224a1006c4c023108ee
Ivan Guimaraes Farias <iguimaraesf@gmail.com>
2024-07-14 11:40:00 -0300

    feat: :art: Eventos de domínio
    2 eventos adicionados ao cliente.

--- src/domain/customer/entity/customer.spec.ts
+++ src/domain/customer/entity/customer.spec.ts
@@ -4,21 +4,21 @@ import Customer from "./customer"
 describe("Customer unit tests", () => {
     it("should throw error when id is empty", () => {
         expect(() => {
-            let customer = new Customer("", "Jhon")
+            let customer = Customer.create("", "Jhon")
         }).toThrow("Id is required")
     })
     it("should throw error when name is empty", () => {
         expect(() => {
-            let customer = new Customer("123", "")
+            let customer = Customer.create("123", "")
         }).toThrow("Name is required")
     })
     it("should change the name", () => {
-        const customer = new Customer("123", "John")
+        const customer = Customer.create("123", "John")
         customer.changeName("Jane")
         expect(customer.name).toBe("Jane")
     })
     it("should activate a customer", () => {
-        const customer = new Customer("1", "Fabio")
+        const customer = Customer.create("1", "Fabio")
         const address = new Address("street 1", 123, "01001-000", "São Paulo")
         customer.changeAddress(address)
 
@@ -26,14 +26,14 @@ describe("Customer unit tests", () => {
         expect(customer.isActive()).toBeTruthy()
     })
     it("should throw error when the address is undefined when you activate a customer", () => {
-        const customer = new Customer("2", "Paola")
+        const customer = Customer.create("2", "Paola")
 
         expect(() => {
             customer.activate()
         }).toThrow("Address is mandatory to activate a custumer")
     })
     it("should deactivate a customer", () => {
-        const customer = new Customer("1", "Fabio")
+        const customer = Customer.create("1", "Fabio")
 
         customer.deactivate()
         expect(customer.isActive()).toBeFalsy()
--- src/infrastructure/customer/repository/sequelize/customer.repository.spec.ts
+++ src/infrastructure/customer/repository/sequelize/customer.repository.spec.ts
@@ -25,7 +25,7 @@ describe("Teste do repositório de cliente", () => {
 
     it("deve criar um cliente", async() => {
         const customerRepository = new CustomerRepository()
-        const customer = new Customer("1", "customer 1")
+        const customer = Customer.create("1", "customer 1")
         const address = new Address("street 1", 1, "Zipcode 1", "City 1")
         customer.changeAddress(address)
         await customerRepository.create(customer)
@@ -46,7 +46,7 @@ describe("Teste do repositório de cliente", () => {
 
     it("deve atualizar o cliente", async() => {
         const customerRepository = new CustomerRepository()
-        const customer = new Customer("1", "customer 1")
+        const customer = Customer.create("1", "customer 1")
         const address = new Address("street 1", 1, "Zipcode 1", "City 1")
         customer.changeAddress(address)
         await customerRepository.create(customer)
@@ -70,7 +70,7 @@ describe("Teste do repositório de cliente", () => {
 
     it("deve buscar um cliente", async() => {
         const customerRepository = new CustomerRepository()
-        const customer = new Customer("1", "customer 1")
+        const customer = Customer.create("1", "customer 1")
         const address = new Address("street 1", 1, "Zipcode 1", "City 1")
         customer.changeAddress(address)
         await customerRepository.create(customer)
@@ -101,17 +101,26 @@ describe("Teste do repositório de cliente", () => {
     it("deve buscar todos os clientes", async() => {
         const address = new Address("street 1", 1, "Zipcode 1", "City 1")
         const customerRepository = new CustomerRepository()
-        const customer1 = new Customer("1", "customer 1")
+        const customer1 = Customer.create("1", "customer 1")
         customer1.changeAddress(address)
         await customerRepository.create(customer1)
-        const customer2 = new Customer("2", "customer 2")
+        const customer2 = Customer.create("2", "customer 2")
         customer2.changeAddress(address)
         await customerRepository.create(customer2)
 
         const foundCustomers = await customerRepository.findAll()
+        const removeEvents = (customer: Customer) => {
+            const customerWithoutEvents = { ...customer };
+            delete customerWithoutEvents.events;
+            return customerWithoutEvents;
+        }
 
-        expect(foundCustomers).toHaveLength(2)
-        expect(foundCustomers).toContainEqual(customer1)
-        expect(foundCustomers).toContainEqual(customer2)
+        const foundCustomersWithoutEvents = foundCustomers.map(removeEvents);
+        const customer1WithoutEvents = removeEvents(customer1);
+        const customer2WithoutEvents = removeEvents(customer2);
+
+        expect(foundCustomersWithoutEvents).toHaveLength(2);
+        expect(foundCustomersWithoutEvents).toContainEqual(customer1WithoutEvents);
+        expect(foundCustomersWithoutEvents).toContainEqual(customer2WithoutEvents);
     })
 })
