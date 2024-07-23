25df9f1c6154a8f7c2e64a47eb9ae523141078aa
Ivan Guimaraes Farias <iguimaraesf@gmail.com>
2024-07-23 16:53:23 -0300

    refactor: :wastebasket: remoção de classes não utilizadas

--- src/domain/customer/service/customer.service.ts
+++ src/domain/customer/service/customer.service.ts
@@ -1,5 +1,4 @@
 import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
-import { Mediator } from "../../@shared/service/mediator";
 import Address from "../value-object/address";
 import CustomerFactory from "../factory/customer.factory";
 import { CustomerInterface } from "../entity/customer.interface";
@@ -8,20 +7,15 @@ export default class CustomerService {
 
     constructor(
         private customerRepo: CustomerRepository, 
-        private mediator: Mediator
     ) {
     }
 
-    async create(name: string, address: Address) {
-        const customer = CustomerFactory.create(name)
-        customer.defineAddress(address)
+    async create(customer: CustomerInterface) {
         this.customerRepo.create(customer)
-        //this.mediator.publish(customer)
         return customer
     }
 
     async update(customer: CustomerInterface) {
         this.customerRepo.update(customer)
-        //this.mediator.publish(customer)
     }
 }
